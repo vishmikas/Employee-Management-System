c70d8b5b263bed8149791d2e89356de58a033276
Dilum Samarathunga <75325717+vishmikas@users.noreply.github.com>
2025-12-23 19:22:20 +0530

    created EmployeeManager Interface

---
+++ src/EmployeeManager.java
@@ -0,0 +1,11 @@
+public interface EmployeeManager {
+
+    void addEmployee();
+    void viewEmployee();
+    void searchEmployee();
+    void updateEmployee();
+    void deleteEmployee();
+    void saveToFile();
+    void loadFromFile();
+
+}
